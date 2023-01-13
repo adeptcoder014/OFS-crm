fea59433743eb14cb38ce9fb9af2df2a523d82fc
Nischal <gupta.nischal014@gmail.com>
2023-01-13 19:24:22 +0530

    react-three-fiber budd !@#$

--- components/3dCube.js
+++ components/3dCube.js
@@ -1,6 +1,9 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Children, useRef } from "react";
 import { OrbitControls } from "@react-three/drei";
+import { Physics, useBox, usePlane } from "@react-three/cannon";
+import { functionsIn } from "lodash";
+
 //==========================
 
 function Spin({ children }) {
@@ -12,9 +15,25 @@ function Spin({ children }) {
   return <group ref={ref}>{children}</group>;
 }
 
-function Dabba(props) {
+//--------------------
+function Plane(props) {
+  const [ref] = usePlane(() => ({ ...props }));
   return (
-    <mesh {...props}>
+    <mesh ref={ref}>
+      <planeGeometry args={[10, 10]} />
+      <meshBasicMaterial color="orange" />
+    </mesh>
+  );
+}
+//--------------------------------
+function Dabba(props) {
+  const [ref] = useBox(() => ({
+    mass: 1,
+    ...props,
+  }));
+
+  return (
+    <mesh ref={ref}>
       <boxGeometry />
       <meshStandardMaterial color="gray" />
     </mesh>
@@ -25,14 +44,17 @@ export default function Cube() {
     <Canvas>
       <ambientLight />
       <pointLight position={[5, 10, 15]} />
-      <Dabba position={[5, 1, 1]} />
-      <Spin>
-        <Dabba position={[1.5, 0, 1]} />
-      </Spin>
-      <Spin>
-        <Dabba position={[3, 1, 1]} />
-      </Spin>
-      <OrbitControls/>
+      <Physics>
+        <Dabba position={[1, 5, 1]} rotation={[4, -15, 5]} />
+        <Spin>
+          <Dabba position={[1.5, 0, 1]} rotation={[0.4, -10, -5]} />
+        </Spin>
+        <Spin>
+          <Dabba position={[0, 6, 5]} />
+        </Spin>
+        <Plane rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]} />
+        <OrbitControls />
+      </Physics>
     </Canvas>
   );
 }
