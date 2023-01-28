dac7a767b3d92477bea8ef2c48a3cf039c4f4a79
Nischal <gupta.nischal014@gmail.com>
2023-01-28 16:47:53 +0530

    Done

--- pages/index.js
+++ pages/index.js
@@ -19,39 +19,61 @@ export default function Home() {
   //================================
   return (
     <>
-      <h1>Welcome to the OFS CRM system</h1>
-      <Box sx={{ display: "flex" }}>
+      <Box
+        sx={{
+          height: "100vh",
+          display: "flex",
+          // backgroundImage: `url('/bg.jpg')`,
+          backgroundImage: `url("/bg.jpg")`,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <h1>Welcome to the OFS CRM system</h1>
         <Box
           sx={{
-            backgroundImage: "linear-gradient(45deg, #ffa386, #d5552c)",
-            color: "white",
-            fontSize: 18,
-            // width: "15%",
-            textAlign: "center",
-            p: 1,
-            borderRadius: 1,
-            cursor: "pointer",
-            mr: 5,
-            ml: 5,
+            display: "flex",
+            // backgroundImage: `url('/bg.jpg')`,
+            display: "flex",
+            flexDirection: "column",
+            m: "auto",
           }}
-          onClick={() => router.push("/user/login")}
         >
-          User LOGIN
-        </Box>
-        <Box
-          sx={{
-            backgroundImage: "linear-gradient(45deg, #ffa386, #d5552c)",
-            color: "white",
-            fontSize: 18,
-            // width: "15%",
-            textAlign: "center",
-            p: 1,
-            borderRadius: 1,
-            cursor: "pointer",
-          }}
-          onClick={() => router.push("/admin/login")}
-        >
-          Admin LOGIN
+          <Box
+            sx={{
+              backgroundImage: "linear-gradient(45deg, #ffa386, #d5552c)",
+              color: "white",
+              fontSize: 18,
+              // width: "15%",
+              textAlign: "center",
+              p: 1,
+              borderRadius: 1,
+              cursor: "pointer",
+              mr: 5,
+              ml: 5,
+              height: 50,
+            }}
+            onClick={() => router.push("/user/login")}
+          >
+            User LOGIN
+          </Box>
+          <Box
+            sx={{
+              backgroundImage: "linear-gradient(45deg, #ffa386, #d5552c)",
+              color: "white",
+              fontSize: 18,
+              // width: "15%",
+              textAlign: "center",
+              p: 1,
+              borderRadius: 1,
+              cursor: "pointer",
+              height: 50,
+              mt:2
+            }}
+            onClick={() => router.push("/admin/login")}
+          >
+            Admin LOGIN
+          </Box>
         </Box>
       </Box>
     </>
